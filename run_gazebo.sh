afad77d66219e68e548ce70599720e6fd43b19ad
Baozhe Zhang <907711755@qq.com>
2022-05-25 19:49:00 +0800

    added shell script

--- run_gazebo.sh
+++ run_gazebo.sh
@@ -1,2 +1,3 @@
-source devel/setup.bash && 
-roslaunch interbotix_moveit interbotix_moveit.launch robot_name:=wx250s use_gazebo:=true dof:=6 world_name:=warehouse.world
+xterm -e "source ~/.bashrc && source devel/setup.bash && roslaunch interbotix_moveit interbotix_moveit.launch robot_name:=wx250s use_gazebo:=true dof:=6 world_name:=warehouse.world" &
+xterm -e "sleep 5 && source ~/.bashrc && source devel/setup.sh && roslaunch object_color_detector object_detect_hsv.launch use_gazebo:=true" &
+xterm -e "sleep 10 && source ~/.bashrc && source devel/setup.sh && rosrun object_color_detector main_project2.py __ns:=wx250s" &
